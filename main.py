d1650b0b709f0146b6549c0dfb57ae3513e1dd65
aminul-hq <mdaminul94@gmail.com>
2022-09-27 19:06:17 +0600

    single line write complete

--- main.py
+++ main.py
@@ -1,28 +1,28 @@
 import os
 import xlsxwriter
 
-path = "E://Python//cy//cy"
-workbook = xlsxwriter.Workbook(path +"/test.xlsx")
+path = "E:/Python/cy/cy"
+workbook = xlsxwriter.Workbook("./test1.xlsx")
 worksheet = workbook.add_worksheet()
 
 
 def readFileList():
-    row = 0
-    col = 0
+    row = 1
+    title = ["Ocean Name", "Number", "year:month:date:hour", "data type", "", "latitude", "longitude", "wind speed"]
+    worksheet.write_row(0, 0, title)
     dir_list = os.listdir(path)
     for x in dir_list:
         if x.endswith(".txt"):
             with open(path+"/"+x, "r") as filestream:
+                data = []
                 for line in filestream:
                     currentline = line.split(",")
                     currentline[-1] = currentline[-1].strip()
-                    print("row : " + str(row))
-                    print("column : " + str(col))
-                    print("data : " + str(currentline))
-                    worksheet.write_column(row, col, currentline)
-                    col += 1
+                    data.append(currentline)
+                print(data)
+                worksheet.write_row(row, 0, data[0])
                 row += 1
-            row = 0
+
     workbook.close()
 
 
